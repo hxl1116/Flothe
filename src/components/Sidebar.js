fb95b1153fa8e7b72a5f4fbad19bb1a1fc9bc151
Hersh Nagpal <hershnagpal@gmail.com>
2019-11-06 18:10:53 -0500

    Stubbed Sidebar class

---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+class Sidebar extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
