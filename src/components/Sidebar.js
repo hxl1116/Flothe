ee8eea4f17624b99e14d49cff642f2cb7e04ad98
Hersh Nagpal <hershnagpal@gmail.com>
2019-11-10 12:45:19 -0500

    Clicking on profile image toggles sidemenu

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,20 +2,40 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
 class Sidebar extends Component {
+    
     constructor(props) {
         super(props);
         this.state = {
-
+            sidebarOpen:false
         }
     }
+
+    
+
+    toggleSidemenu = () => {
+        var opposite = this.state.sidebarOpen == false ? true : false;
+        this.setState({ sidebarOpen:opposite });
+    }
+
     render() {
+        const style = this.state.sidebarOpen ? {display: 'none'} : {};
         return (
             <div id="sidebar">
                 {/* <button type="button">Sidebar</button> */}
                 <div id="profile-img" style={{
                     backgroundImage: "url(./img/barack_obama.jpg)"
-                }}/>
+                }}
+                onClick={this.toggleSidemenu}
+                />
+
+                <div id="sidemenu" style={style}>
+                    <a class="sidemenu-item">Teams</a>
+                    <a class="sidemenu-item">Profile</a>
+                    <a class="sidemenu-item">Settings</a>
+                </div>
+
             </div>
+            
         )
     }
 }
