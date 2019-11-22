79f271913c4760772d87c7b658f0f9160cb02615
kxl1360 <kelleyylamb@kxl1360.student.rit.edu>
2019-11-21 19:42:49 -0500

    access material icons and update menu icon

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -62,8 +62,8 @@ class Sidebar extends Component {
 
         return (
             <div id="side-bar">
-                <button onClick={this.toggleSideMenu}>Menu</button>
-                {/*Replace with a hamburger menu icon*/}
+                {/*<button onClick={this.toggleSideMenu}>Menu</button>*/}
+                <i className="material-icons md-36" onClick={this.toggleSideMenu}>dehaze</i>
                 <ul id="side-menu" className={this.state.showSideMenu ? 'show' : 'hide'}>
                     <div id="profile-img"/>
                     <div id="team-section">
