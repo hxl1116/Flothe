01508148ff9856524bcafb94b45e71ea966f8ffb
Henry Larson <hxl1116@g.rit.edu>
2019-12-01 22:59:42 -0500

    Today Section files created.

---
+++ src/components/Today.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+class Today extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Today
